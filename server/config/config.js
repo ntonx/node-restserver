253c7197b84cfa2ec23cd004b0b6bcf23bdc6873
ntonx <ntonx9@gmail.com>
2019-12-29 10:16:18 -0600

    Variable de entorno Mongo establecida

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://alligator:MFTxtEABYnPlz7BN@cluster0-l2rzm.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
